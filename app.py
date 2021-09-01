6edcb4655c657f5a7e3855f84dc81d8ccccea672
inarutosai7 <inarutoai@gmail.com>
2021-09-01 13:55:27 +0800

    Modified app.py

--- app.py
+++ app.py
@@ -9,9 +9,12 @@ from linebot.models import MessageEvent, TextMessage, TextSendMessage, AudioMess
 
 app = Flask(__name__)
 
+
 # LINE 聊天機器人的基本資料
 config = configparser.ConfigParser()
 config.read('config.ini')
+print(config)
+
 
 line_bot_api = LineBotApi(config.get('line-bot', 'channel_access_token'))
 handler = WebhookHandler(config.get('line-bot', 'channel_secret'))
@@ -34,55 +37,16 @@ def callback():
 
 
 # 學你說話
-# @handler.add(MessageEvent, message=AudioMessage)
-# def handle_audio(event):
-#     # if event.source.user_id != "Udeadbeefdeadbeefdeadbeefdeadbeef":
-#     #     line_bot_api.reply_message(
-#     #         event.reply_token,
-#     #         TextSendMessage(text=event.message.text)
-#     #     )
-#     # get user & message id
-#     # if event.message.type == 'audio':
-#
-#     user_id = event.source.user_id
-#     message_id = event.message.id
-#     # get image content
-#     audio_content = line_bot_api.get_message_content(message_id)
-#
-#
-#     # audio_content = line_bot_api.get_message_content(event.message.id)
-#     path = './sound.wav'
-#     with open(path, 'wb') as fd:
-#         for chunk in audio_content.iter_content():
-#             fd.write(chunk)
 
-# Other Message Type
-@handler.add(MessageEvent, message=(ImageMessage, VideoMessage, AudioMessage))
+@handler.add(MessageEvent, message=AudioMessage)
 def handle_content_message(event):
-    if isinstance(event.message, ImageMessage):
-        ext = 'jpg'
-    elif isinstance(event.message, VideoMessage):
-        ext = 'mp4'
-    elif isinstance(event.message, AudioMessage):
-        ext = 'm4a'
-    else:
-        return
+
     message_content = line_bot_api.get_message_content(event.message.id)
-    path = './sound.wav'
-    with tempfile.NamedTemporaryFile(path, prefix=ext + '-', delete=False) as tf:
+    path = './static/{}.wav'.format(event.message.id)
+    with open(path , 'wb') as fd:
         for chunk in message_content.iter_content():
-            tf.write(chunk)
-        tempfile_path = tf.name
+            fd.write(chunk)
 
-    dist_path = tempfile_path + '.' + ext
-    dist_name = os.path.basename(dist_path)
-    os.rename(tempfile_path, dist_path)
-
-    line_bot_api.reply_message(
-        event.reply_token, [
-            TextSendMessage(text='Save content.'),
-            TextSendMessage(text=request.host_url + os.path.join('static', 'tmp', dist_name))
-        ])
 
 
 if __name__ == "__main__":
